b7fde3dbfb09bb87fd710837553cbaf775fc1bc1
sarthakpm16 <sarthakpalmahajan@gmail.com>
2025-06-28 19:31:01 -0400

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hi, I'm Sarthak</h1>
+      <p>a 16 year old from DC. currently: making it easier for teachers to understand their students :)</p>
+
+      <h2>exp</h2>
+      <ul>
+        <li>founder @ klinn - quick & easy internship cold emailing for premeds. 14k+ users. partnered w/ ora ai (yc s23), crackd, harvard tech, etc. currently pivoting</li>
+        <li>founding partner @ egglabs - YC for youth orgs</li>
+        <li>exec director @ crabhacks - annual hackathon for high schoolers. raised $4.5k+</li>
+        <li>state officer @ md deca - 1700+ members. also placed top 10 internationally out of 200k+ members</li>
+      </ul>
+
+      <h2>random accomplishments:</h2>
+      <ul>
+        <li>2.5m+ impressions across linkedin & x. growth is my moat</li>
+        <li>was working on an app. failed.</li>
+        <li>sold soccer jerseys in middle school</li>
+      </ul>
+
+      <h2>reach me:</h2>
+      <p>
+        <a href="https://klinn.works">klinn</a> /{' '}
+        <a href="https://linkedin.com/in/sarthak-pal-mahajan">linkedin</a> /{' '}
+        <a href="https://x.com/sarthathak">X</a> /{' '}
+        <a href="https://instagram.com/sarthathak">instagram</a>
+      </p>
+    </main>
+  );
+}
